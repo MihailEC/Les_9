3292a9cc24d174609d39f7a4a025e9d05c20b04b
MihailEC <mihail-ekimov@mail.ru>
2023-10-06 02:51:15 +0300

    VeryGood?

--- loto_classes.py
+++ loto_classes.py
@@ -2,12 +2,74 @@ import random
 
 
 class Player:
-    def __init__(self):
-# TODO: сделать определение человека проще. Возможно сначала просить ввести формат игры. С компьютером или с человеком.
-        self.name = input('Введите имя игрока: ')
-        self.type = input('Введите тип игрока (человек/компьютер): ')
+    def __init__(self, count_players):
+        type_player = input(f'Игрок {count_players} человек или компьютер (ч/к): ')
+        type_player = type_player[:1]
+        type_player = type_player.lower()
+        self.type = type_player
+        if self.type == 'ч':
+            name = input(f'Введите имя игрока {count_players}: ')
+            self.name = name
+        elif self.type == 'к':
+            self.name = f'Компьютер {count_players}'
+        else:
+            raise ValueError
         numbers = [i for i in range(1, 91)]
         self.card_list = random.sample(numbers, 15)
+        self.card_list.sort()
+        self.score = 0
 
-    def change_card(self):
-        pass
+    def change_card(self, choice_dice):
+        for j in range(0, 15):
+            if self.card_list[j] == choice_dice:
+                self.card_list[j] = '-'
+
+    def card_print(self):
+        card_list_1 = self.card_list[:5]
+        card_list_2 = self.card_list[5:10]
+        card_list_3 = self.card_list[10:15]
+        print(f'------Карточка {self.name}------')
+        print(card_list_1)
+        print(card_list_2)
+        print(card_list_3)
+        print('---------------------------------------')
+
+
+def circle_of_players(dice_list, players_list, count_players):
+    """
+    Функция для игры в лото. Проход одного круга по игрокам.
+    :param dice_list: Список цифр в мешке.
+    :param players_list: Список игроков.
+    :param count_players: Количество игроков
+    :return:
+    """
+    choice_dice = random.choice(dice_list)
+    print(f'Новый бочонок: {choice_dice} (осталось: {len(dice_list) - 1})')
+    dice_list.remove(choice_dice)
+    for i in range(0, count_players):
+        # Цикл вывода карточек
+        for j in range(0, count_players):
+            players_list[j].card_print()
+        print(f'Ход игрока {players_list[i].name}.')
+
+        if players_list[i].type == 'ч':
+            action = input('Зачеркнуть цифру?(y/n): ')
+            action = action[:1]
+            action = action.lower()
+            if choice_dice in players_list[i].card_list and action == 'y':
+                players_list[i].change_card(choice_dice)
+                players_list[i].score += 1
+            elif choice_dice in players_list[i].card_list and action == 'n':
+                print(f'Число в карточке есть! Игрок {players_list[i].name} проиграл!')
+                exit()
+            elif choice_dice not in players_list[i].card_list and action == 'n':
+                continue
+            elif choice_dice not in players_list[i].card_list and action == 'y':
+                print(f'Число в карточке нет! Игрок {players_list[i].name} проиграл!')
+                exit()
+            else:
+                print('Неверно введено действие!')
+                exit()
+        elif players_list[i].type == 'к':
+            players_list[i].change_card(choice_dice)
+            players_list[i].score += 1
--- loto_game.py
+++ loto_game.py
@@ -1,60 +1,27 @@
-import random
-
-from loto_classes import Player
-
-player_one = Player()
-card_p_one = player_one.card_list
-player_two = Player()
-card_p_two = player_two.card_list
+import loto_classes
 
 dice_list = [i for i in range(1, 91)]
 
 win_list = ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
 
-while card_p_one != win_list or card_p_two != win_list:
-    # TODO: 1. сделать сортировку по возрастанию для каждой строки чисел в карточке
-    # TODO: 2. сделать функции вывода карточек в классе или отдельно
-    choice_dice = random.choice(dice_list)
-    print(f'Новый бочонок: {choice_dice} (осталось: {len(dice_list) - 1})')
-    dice_list.remove(choice_dice)
-    print(f'------Карточка {player_one.name}------')
-    print(card_p_one[0:4])
-    print(card_p_one[5:9])
-    print(card_p_one[10:15])
-    print('---------------------------------------')
+try:
+    count_players = int(input('Введите количество игроков: '))
+except ValueError:
+    print('Введите число! Выходим.')
+    exit()
 
-    print(f'------Карточка {player_two.name}------')
-    print(card_p_two[0:4])
-    print(card_p_two[5:9])
-    print(card_p_two[10:15])
-    print('---------------------------------------')
-    action = input('Зачеркнуть цифру?(y/n): ')
-# TODO: добавить метод изменения карточки в классе
-    if player_one.type == 'человек' and action == 'y':
-        if choice_dice in card_p_one:
-            for j in range(len(card_p_one)):
-                if card_p_one[j] == choice_dice:
-                    card_p_one[j] = '-'
-        else:
-            print(f'Числа в карточке нет! Игрок {player_one.name} проиграл!')
+players_list = []
+try:
+    for i in range(1, count_players + 1):
+        players_list.append(loto_classes.Player(i))
+except ValueError:
+    print('Неверно введен тип игрока! Выходим')
+    exit()
+
+# выполняем
+for i in range(1, 91):
+    loto_classes.circle_of_players(dice_list, players_list, count_players)
+    for j in range(0, count_players):
+        if players_list[j].card_list == win_list:
+            print(f'Победил игрок {players_list[j].name}. Количество очков: {players_list[j].score}')
             break
-    elif player_two.type == 'человек' and action == 'y':
-        if choice_dice in card_p_two:
-            for j in range(len(card_p_two)):
-                if card_p_two[j] == choice_dice:
-                    card_p_two[j] = '-'
-        else:
-            print(f'Числа в карточке нет! Игрок {player_two.name} проиграл!')
-            break
-    elif player_one.type == 'компьютер':
-        for j in range(len(card_p_one)):
-            if card_p_one[j] == choice_dice:
-                card_p_one[j] = '-'
-    elif player_two.type == 'компьютер':
-        for j in range(len(card_p_two)):
-            if card_p_two[j] == choice_dice:
-                card_p_two[j] = '-'
-    else:
-        print('Тип игрока не определен!')
-        break
-# TODO: сделать подсчет количества очков и вывод победителя
